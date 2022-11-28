c4b39c2a41e3b7c841ac6061e0e57fbe35715922
Sergey <s.gz@yandex.ru>
2022-11-28 12:42:35 +0300

    Создали класс Circle

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ class Rectangle:
 
     def area(self):
         return self.a * self.b
-    
+
     def get_height(self):
         return self.a
-    
+
     def get_width(self):
         return self.b
 
+
+class Circle:
+    def __init__(self, r):
+        self.r = r
+
+
 main_rect = Rectangle(5, 4)
 print(main_rect.area())
+main_circle = Circle(2)
