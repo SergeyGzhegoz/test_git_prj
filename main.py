f6c4b193d33432ac9f2ffd217c4b6bcf16ff14b3
Sergey <s.gz@yandex.ru>
2022-11-28 12:38:10 +0300

    Написали методы set и perimeter

--- main.py
+++ main.py
@@ -5,12 +5,23 @@ class Rectangle:
 
     def area(self):
         return self.a * self.b
-    
+
     def get_height(self):
         return self.a
-    
+
     def get_width(self):
         return self.b
 
+    def set_height(self, a):
+        self.a = a
+
+    def set_width(self, b):
+        self.b = b
+
+    def perimeter(self):
+        return self.a * 2 + self.b * 2
+
+
 main_rect = Rectangle(5, 4)
 print(main_rect.area())
+print(main_rect.perimeter())
